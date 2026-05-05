48b6e02aaf45d2b7a513f14ec2a2b04a8f98c6bb
Ripan Pramanik <ripan_pramanick@hotmail.com>
2026-05-05 21:21:53 +0530

    feat: implement axios API services and configure project build settings

--- kaalchakra/client/vite.config.js
+++ kaalchakra/client/vite.config.js
@@ -1,32 +1,4 @@
-// // client/vite.config.js
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5174,
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:5000',
-//         changeOrigin: true,
-//         secure: false,
-//         rewrite: (path) => path,
-//         configure: (proxy, options) => {
-//           proxy.on('error', (err, req, res) => {
-//             console.log('proxy error', err);
-//           });
-//           proxy.on('proxyReq', (proxyReq, req, res) => {
-//             console.log('Sending Request to the Target:', req.method, req.url);
-//           });
-//           proxy.on('proxyRes', (proxyRes, req, res) => {
-//             console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-//           });
-//         }
-//       }
-//     }
-//   }
-// });
 
 // client/vite.config.js
 import { defineConfig } from 'vite'
@@ -38,7 +10,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://kaalchakra-backend.onrender.com',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path,
