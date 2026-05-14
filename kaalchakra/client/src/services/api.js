afaaa8035da47d6b346cc1bfbf5868f7eee9be0c
Ripan Pramanik <ripan_pramanick@hotmail.com>
2026-05-14 21:37:07 +0530

    feat: create axios instance with request and response interceptors for API communication

--- kaalchakra/client/src/services/api.js
+++ kaalchakra/client/src/services/api.js
@@ -1,8 +1,8 @@
 // client/src/services/api.js
 import axios from 'axios';
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://kaalchakra-backend.onrender.com/api';
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://kaalchakra-backend.onrender.com/api';
+// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
